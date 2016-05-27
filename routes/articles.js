ecad4e8841ec3a2b68992136dd630e68d68ddf68
Methew Guda <methewguda@gmail.com>
2016-05-27 20:07:43 -0300

    Added routes for articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET articles listing. */
+var article = require('../models/article');
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  article.getArticles(function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles);
+
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  article.getArticleById(req.params.id, function(err, article){
+    if(err){
+      console.log(err);
+    }
+    res.json(article);
+
+  });
+});
+
+router.get('/category/:category', function(req, res, next) {
+  article.getArticlesByCategory(req.params.category, function(err, articles){
+    if(err){
+      console.log(err);
+    }
+    res.json(articles);
+
+  });
 });
 
 module.exports = router;
