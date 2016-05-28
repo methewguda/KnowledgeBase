d7cd7e427742e8772247fa66dc49efa5cb0bf162
Methew Guda <methewguda@gmail.com>
2016-05-28 02:56:17 -0300

    Added Controllers

---
+++ public/controllers/articles.controllers.js
@@ -0,0 +1,73 @@
+angular.module('kB')
+
+    .controller('ArticlesCtrl', ['$scope', '$http', function ($scope, $http) {
+        $http.get('/articles').success(function (data) {
+            $scope.articles = data;
+        });
+    }])
+
+    .controller('ArticleCategoryCtrl', ['$scope', '$http','$routeParams', function ($scope, $http, $routeParams) {
+        $http.get('/articles/category/' + $routeParams.category).success(function (data) {
+            $scope.cat_articles = data;
+            $scope.category = $routeParams.category;
+        });
+    }])
+
+    .controller('ArticleDetailsCtrl', ['$scope', '$http','$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+        $http.get('/articles/' + $routeParams.id).success(function(data){
+            $scope.article = data;
+        });
+
+        $scope.removeArticle = function(){
+            $http.delete('/articles/' + $routeParams.id).success(function(data){
+                console.log(data);
+            });
+
+            $location.path('/articles');
+        }
+    }])
+
+    .controller('ArticleCreateCtrl', ['$scope', '$http','$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+        $http.get('/categories').success(function(data){
+            $scope.categories = data;
+        });
+
+        $scope.addArticle= function(){
+            var data = {
+                title:      $scope.title,
+                body:       $scope.body,
+                category:   $scope.category
+            }
+
+            $http.post('/articles', data).success(function(data, status){
+                console.log(status);
+            });
+
+            $location.path('/articles');
+        }
+    }])
+
+    .controller('ArticleEditCtrl', ['$scope', '$http','$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+        $http.get('/categories').success(function(data){
+            $scope.categories = data;
+        });
+
+        $http.get('/articles/' + $routeParams.id).success(function(data){
+            $scope.article = data;
+        });
+
+        $scope.updateArticle= function(){
+            var data = {
+                id:         $routeParams.id,
+                title:      $scope.article.title,
+                body:       $scope.article.body,
+                category:   $scope.article.category
+            }
+
+            $http.post('/articles', data).success(function(data, status){
+                console.log(status);
+            });
+
+            $location.path('/articles');
+        }
+    }]);
