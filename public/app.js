2023e6e5496e0b67c0f94024b900272c9e620058
Methew Guda <methewguda@gmail.com>
2016-05-28 02:57:17 -0300

    Updated Knowledgebase

---
+++ public/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('kB', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+        when('/categories',{
+        templateUrl: 'views/categories.view.html',
+        controller: 'CategoriesCtrl'
+    }).
+    when('/articles',{
+        templateUrl: 'views/articles.view.html',
+        controller: 'ArticlesCtrl'
+    }).
+    when('/articles/details/:id',{
+        templateUrl: 'views/article_details.view.html',
+        controller: 'ArticleDetailsCtrl'
+    }).
+    when('/articles/category/:category',{
+        templateUrl: 'views/cat_articles.view.html',
+        controller: 'ArticleCategoryCtrl'
+    }).
+    when('/articles/add',{
+        templateUrl: 'views/add_article.view.html',
+        controller: 'ArticleCreateCtrl'
+    }).
+    when('/articles/edit/:id',{
+        templateUrl: 'views/edit_article.view.html',
+        controller: 'ArticleEditCtrl'
+    }).
+    otherwise({redirectTo: 'categories'})
+}]);
