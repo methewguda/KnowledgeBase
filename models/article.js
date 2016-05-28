71dea831a6d0a34da50bacd18b212e35853138f7
Methew Guda <methewguda@gmail.com>
2016-05-27 21:51:25 -0300

    Modified model for articles

--- models/article.js
+++ models/article.js
@@ -38,3 +38,34 @@ module.exports.getArticlesByCategory= function(category, callback){
     var query = {category: category};
     Article.find(query, callback);
 }
+
+//Add an article
+module.exports.createArticle = function(newArticle, callback){
+    newArticle.save(callback);
+}
+
+//Update an article
+module.exports.updateArticle = function(id, data, callback){
+    var title = data.title;
+    var body = data.body;
+    var category = data.category;
+
+    var query = {_id: id};
+    Article.findById(id, function(err, article){
+        if(!article){
+            return next(new Error('Could not load the article.'));
+        }else{
+            article.title = title;
+            article.body = body;
+            article.category = category;
+
+            article.save(callback);
+        }
+    });
+}
+
+//Remove an Article
+module.exports.createArticle = function(id, callback){
+    Article.find({_id: id}).remove(callback);
+}
+
